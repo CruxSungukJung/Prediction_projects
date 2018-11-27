1118b7f7552f7da5beac01a90d6d165133d78389
sungukjung <su_j615@naver.com>
2018-11-27 18:24:21 +0900

    mnist abstraction using dnn_classifier

--- abstraction_basic/abstraction_lenear_regression_basic.py
+++ abstraction_basic/abstraction_lenear_regression_basic.py
@@ -5,11 +5,10 @@ from sklearn.datasets import load_boston
 from sklearn import datasets,metrics,preprocessing
 boston = load_boston()
 
-x_data = preprocessing.StandardScaler().fit_transform(boston.data)
-y_data = boston.target
-x = tf.placeholder(tf.float64,shape = [None,13])
-y_true = tf.placeholder(tf.float64,shape = None)
 
+x_data = preprocessing.StandardScaler().fit_transform(boston.data)
+
+"""
 with tf.name_scope('inference') as scope:
     w = tf.Variable(tf.zeros([1,13],dtype=tf.float64,name="weight"))
     b = tf.Variable(0,dtype=tf.float64,name="bias")
@@ -31,3 +30,24 @@ with tf.Session() as sess:
         
         print(mse)
         
+        
+        
+        abstraction begin! ㅎㅎ
+        ㅁ
+        ㅁ
+        ㅁ
+        ㅁ
+        ㅁ
+        ㅁ
+        ㅁ
+        V
+"""
+NUM_STEP = 200
+MINI_BATCH = 506
+feature_columns = learn.infer_real_valued_columns_from_input(x_data)
+reg = learn.LinearRegressor(feature_columns=feature_columns,
+                            optimizer= tf.train.GradientDescentOptimizer(learning_rate=0.1))
+
+reg.fit(x_data,boston.target,steps=NUM_STEP,batch_size=MINI_BATCH)
+MSE = reg.evaluate(x_data,boston.target,steps=1)
+print(MSE)
---
+++ abstraction_basic/minist_abstraction.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+from tensorflow.contrib import learn
+import numpy as np
+import sys
+from tensorflow.examples.tutorials.mnist import input_data
+
+
+mnist = input_data.read_data_sets("/tmp/data/", one_hot=False)
+x_data,y_data = mnist.train.images,mnist.train.labels.astype(np.int32)
+x_test,y_test = mnist.test.images,mnist.test.labels.astype(np.int32)
+
+NUM_STEP = 2000
+MINI_BATCH = 128
+
+feature_col = learn.infer_real_valued_columns_from_input(x_data)
+
+dnn = learn.DNNClassifier(
+        feature_columns=feature_col,
+        hidden_units=[200],
+        n_classes=10,
+        optimizer=tf.train.AdamOptimizer(learning_rate=0.05))
+
+dnn.fit(x=x_data,y=y_data,steps=NUM_STEP,batch_size=MINI_BATCH)
+
+test_acc = dnn.evaluate(x=x_test,y=y_test,steps=1)["accuracy"]
+print('{}'.format(test_acc))
