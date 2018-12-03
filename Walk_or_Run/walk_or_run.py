7db8a330377997490c4a63bca9946a701936f53e
sungukjung <su_j615@naver.com>
2018-12-03 15:13:59 +0900

    run and run and walk is complete!

--- Walk_or_Run/walk_or_run.py
+++ Walk_or_Run/walk_or_run.py
@@ -1,23 +1,64 @@
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
-
-x = tf.placeholder(tf.float32,[None,784])
-y = tf.placeholder(tf.float32,[None,2])
-
-run_label = 1
-walk_lable = 0
-file_address = 'train/run/'
-def _parse_function(filename, label):
-  image_string = tf.read_file(filename)
-  image_decoded = tf.image.decode_jpeg(image_string)
-  image_resized = tf.image.resize_images(image_decoded, [28, 28])
-  return image_resized, label
-
-test_img,test_label = _parse_function(file_address+'run_0a9284fb.png',run_label)
+from walk_or_run_get_test_train import *
+import tflearn 
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.conv import conv_2d, max_pool_2d
+from tflearn.layers.normalization import local_response_normalization
+from tflearn.layers.estimator import regression
 
 
-sess = tf.Session()
 
-sess.run(tf.global_variables_initializer())
-img = sess.run(test_img)
+### image data shape is (224,224,4)
+with tf.Session() as sess:
+    
+    X,Y= sess.run(train_img) ,train_labels
+    X = np.reshape(X,[-1,224,224,4])
+    Y = np.array(Y)
+    X_test, Y_test= sess.run([test_img]),test_label
+    X_test=np.reshape(X_test,[-1,224,224,4])
+    Y_test= np.array(Y_test)
+    
+    
+
+
+
+def CNN_():
+    CNN = input_data(shape=[None, 224, 224, 4], name='input_x')
+    CNN = conv_2d(CNN,128,4,activation='relu', regularizer="L2")
+    CNN = max_pool_2d(CNN,2)
+    CNN = dropout(CNN,keep_prob=0.7)
+    CNN = local_response_normalization(CNN)
+    
+    #layer 1 size:56
+    CNN = conv_2d(CNN,97,11,activation='relu', regularizer="L2")
+    CNN = max_pool_2d(CNN,2)
+    CNN = dropout(CNN,keep_prob=0.5)
+    CNN = local_response_normalization(CNN)
+    ###layer 2 size 56
+        #layer 1 size:56
+    CNN = conv_2d(CNN,97,4,activation='relu', regularizer="L2")
+    CNN = max_pool_2d(CNN,2)
+    CNN = dropout(CNN,keep_prob=0.5)
+    CNN = local_response_normalization(CNN)
+    
+    
+    CNN = conv_2d(CNN,30,2,activation='relu', regularizer="L2")
+    CNN = max_pool_2d(CNN,2)
+    CNN = dropout(CNN,keep_prob=0.5)
+    CNN = local_response_normalization(CNN)
+    
+    
+    CNN = conv_2d(CNN,5,2,activation='relu', regularizer="L2")
+    CNN = max_pool_2d(CNN,2)
+    CNN = dropout(CNN,keep_prob=0.5)
+    #layer 3 size :28
+    return CNN
+
+fc_layer = fully_connected(CNN_(),2,activation='softmax')
+output = regression(fc_layer,optimizer='adam',learning_rate=0.0007,loss='categorical_crossentropy',name='targets')
+
+model = tflearn.DNN(output,tensorboard_verbose=0,tensorboard_dir = './walk_run',checkpoint_path = './walk_run/checkpoint')
+model.fit({'input_x':X},{'targets':Y},n_epoch=15,validation_set=({'input_x':X_test},{'targets':Y_test}))
+model.evaluate({'input_x':X_test},{'targets':Y_test})
--- abstraction_basic/minist_abstraction.py
+++ abstraction_basic/minist_abstraction.py
@@ -2,8 +2,9 @@ import tensorflow as tf
 from tensorflow.contrib import learn
 import numpy as np
 import sys
+import matplotlib.pyplot as plt
 from tensorflow.examples.tutorials.mnist import input_data
-
+from sklearn.metrics import confusion_matrix
 
 mnist = input_data.read_data_sets("/tmp/data/", one_hot=False)
 x_data,y_data = mnist.train.images,mnist.train.labels.astype(np.int32)
@@ -25,3 +26,8 @@ dnn.fit(x=x_data,y=y_data,steps=NUM_STEP,batch_size=MINI_BATCH)
 test_acc = dnn.evaluate(x=x_test,y=y_test,steps=1)["accuracy"]
 print('{}'.format(test_acc))
 
+y_pred = dnn.predict(x=x_test,as_iterable=False)
+class_names = ['0','1','2','3','4','5','6','7','8','9']
+cnf_matrix = confusion_matrix(y_test,y_pred)
+
+plt.plot(cnf_matrix)
